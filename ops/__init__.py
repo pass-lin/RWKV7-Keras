afb6924b9b6c02e0023f6e568d0ee03b827259ee
pass_lin <935499957@qq.com>
2025-03-23 01:49:29 +0800

    torch cpu下使用native算子

--- ops/__init__.py
+++ ops/__init__.py
@@ -1,6 +1,11 @@
 import keras
 
 if keras.config.backend() == "torch":
-    from ops.torch_op import RWKV7_OP
+    import torch
+    if torch.cuda.is_available():
+        from ops.torch_op import RWKV7_OP
+    else:
+        from ops.native_keras_op import RWKV7_OP
+    
 else:
     from ops.native_keras_op import RWKV7_OP
